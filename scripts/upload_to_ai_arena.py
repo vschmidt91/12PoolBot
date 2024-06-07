4808040909ee383307a77593146d9e365d1f4571
Volker Schmidt <volker.schmidt91@gmail.com>
2024-06-07 19:45:43 +0200

    Add Readme to bot upload

--- scripts/upload_to_ai_arena.py
+++ scripts/upload_to_ai_arena.py
@@ -1,24 +1,32 @@
 import os
 import requests
 
-API_TOKEN_ENV: str = "AI_ARENA_API_TOKEN"
-BOT_ID_ENV: str = "AI_ARENA_BOT_ID"
+API_TOKEN_ENV: str = "UPLOAD_API_TOKEN"
+BOT_ID_ENV: str = "UPLOAD_BOT_ID"
 ZIPFILE_NAME: str = "bot.zip"
+README_FILE_NAME: str = "README.md"
 
 token = os.environ.get(API_TOKEN_ENV)
 bot_id = os.environ.get(BOT_ID_ENV)
-print(bot_id, token)
 url = f'https://aiarena.net/api/bots/{bot_id}/'
-with open(ZIPFILE_NAME, "rb") as bot_zip:
-    req_data = {
+
+print("Uploading bot")
+with (
+    open(ZIPFILE_NAME, "rb") as bot_zip,
+    open(README_FILE_NAME, "rb") as readme,
+):
+    request_headers = {
+        "Authorization": f"Token {token}",
+    }
+    request_data = {
         "bot_zip_publicly_downloadable": True,
-        # "bot_data_publicly_downloadable": False,
-        # "bot_data_enabled": True,
-        # "wiki_article_content": descriptions[name],
+        "bot_data_publicly_downloadable": False,
+        "bot_data_enabled": False,
+        "wiki_article_content": readme,
     }
-    req_files = {
-        "bot_zip": bot_zip
+    request_files = {
+        "bot_zip": bot_zip,
     }
-    r = requests.patch(url, headers={"Authorization": f"Token {token}"}, data=req_data, files=req_files)
-    print(r)
-    print(r.content)
+    response = requests.patch(url, headers=request_headers, data=request_data, files=request_files)
+    print(response)
+    print(response.content)
