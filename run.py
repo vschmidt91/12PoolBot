fb47a7708864f252ab963baea8d626c5df0c8078
Volker Schmidt <volker.schmidt91@gmail.com>
2024-06-08 17:36:05 +0200

    Revert accidental change

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ sys.path.append("ares-sc2")
 
 import yaml
 
-from main import TwelvePoolBot
+from bot.main import TwelvePoolBot
 from ladder import run_ladder_game
 
 # change if non default setup / linux
