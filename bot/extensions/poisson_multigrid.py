968175492bc644c6cb3b3ef5e449820ce5784e95
Volker Schmidt <volker.schmidt91@gmail.com>
2024-06-10 23:23:35 +0200

    optimize multigrid settings

--- bot/extensions/poisson_multigrid.py
+++ bot/extensions/poisson_multigrid.py
@@ -1,18 +1,25 @@
-import enum
-import functools
-import itertools
-import math
+from enum import Enum, auto
+from dataclasses import dataclass
+from functools import cache
 import typing
 
-import numpy
+import numpy as np
 import scipy.signal
-import skimage.measure
-import skimage.transform
+from skimage.measure import block_reduce
 
-class MultigridMode(enum.Enum):
-    VCycle = enum.auto()
-    WCycle = enum.auto()
-    FCycle = enum.auto()
+class MultigridMode(Enum):
+    VCycle = auto()
+    WCycle = auto()
+    FCycle = auto()
+
+
+@dataclass
+class MultigridOptions:
+    presmooth: int = 2
+    postsmooth: int = 1
+    gsrb: bool = False
+    reduction: int = 2
+    mean_reduction: bool = True
 
 
 SUB_CYCLES = {
@@ -21,171 +28,152 @@ SUB_CYCLES = {
     MultigridMode.FCycle: [MultigridMode.FCycle, MultigridMode.VCycle],
 }
 
-JACOBI_KERNEL = numpy.array([
+
+def prolongate(array, block_size):
+    tmp_shape = np.array((array.shape, block_size)).T.flatten()
+    tmp = np.empty(tmp_shape, array.dtype)
+    indexer = tuple([slice(None), None] * array.ndim)
+    tmp[...] = array[indexer]
+    out_shape = np.multiply(array.shape, block_size)
+    out = tmp.reshape(out_shape)
+    return out
+
+
+def smooth_jacobi(
+    x: np.ndarray,
+    rhs: np.ndarray,
+    b: np.ndarray,
+    n: np.ndarray,
+    h2: float,
+    omega: float = 4/5,
+) -> np.ndarray:
+
+    s = convolve_jacobi(b * x)
+    # s2 = s + (4 - n) * x
+    # x2 = (s2 + h2 * rhs) / 4
+    x2 = (s + h2 * rhs) / np.maximum(1e-10, n)
+    x3 = (1 - omega) * x + omega * x2
+    return x3
+
+
+@cache
+def _get_checkerboard(shape):
+    return np.indices(shape).sum(axis=0) % 2
+
+
+JACOBI_KERNEL = np.array([
     [0, 1, 0],
     [1, 0, 1],
     [0, 1, 0],
 ])
 
-def prolongate(array, block_size):
-    tmp_shape = [
-        d
-        for p in zip(array.shape, block_size)
-        for d in p
-    ]
-    tmp = numpy.empty(tmp_shape, array.dtype)
-    indexer = tuple(
-        d
-        for _ in range(len(array.shape))
-        for d in (slice(None), None)
-    )
-    tmp[...] = array[indexer]
-    out_shape = [numpy.prod(p) for p in zip(array.shape, block_size)]
-    out = tmp.reshape(*out_shape)
-    return out
 
-def smooth_jacobi(
-    x: numpy.ndarray,
-    rhs: numpy.ndarray,
-    b: numpy.ndarray,
-    n: numpy.ndarray,
-    h2: float,
-    omega: float = 4/5,
-) -> numpy.ndarray:
+def convolve(A: np.ndarray, k: np.ndarray) -> np.ndarray:
+    return scipy.ndimage.convolve(A, k)
 
-    s = convolve(b * x, JACOBI_KERNEL)
-    # s2 = s + (4 - n) * x
-    # x2 = (s2 + h2 * rhs) / 4
-    x2 = (s + h2 * rhs) / numpy.maximum(1e-10, n)
-    x3 = (1 - omega) * x + omega * x2
-    return x3
+def convolve_jacobi(A: np.ndarray) -> np.ndarray:
+    Ap = np.pad(A, ((1, 1), (1, 1)))
+    cp = np.roll(Ap, -1, 0) + np.roll(Ap, +1, 0) + np.roll(Ap, -1, 1) + np.roll(Ap, +1, 1)
+    c = cp[1:-1,1:-1]
+    return c
 
-def gsrb_standalone(
-    x: numpy.ndarray,
-    rhs: numpy.ndarray,
-    b: numpy.ndarray,
-):
-    return smooth_gsrb(x, rhs, b, convolve(b, JACOBI_KERNEL), 1.0)
-
-
-@functools.cache
-def _get_checkerboard(shape):
-    return numpy.indices(shape).sum(axis=0) % 2
-
-def convolve(A: numpy.ndarray, k: numpy.ndarray) -> numpy.ndarray:
-    while len(k.shape) < len(A.shape):
-        k = numpy.expand_dims(k, -1)
-    return scipy.ndimage.convolve(A, k, mode="nearest")
 
 def smooth_gsrb(
-    x: numpy.ndarray,
-    rhs: numpy.ndarray,
-    b: numpy.ndarray,
-    n: numpy.ndarray,
+    x: np.ndarray,
+    rhs: np.ndarray,
+    b: np.ndarray,
+    n: np.ndarray,
     h2: float,
-) -> numpy.ndarray:
+) -> np.ndarray:
     checkerboard = _get_checkerboard(x.shape)
     for c in (0, 1):
-        s = convolve(b * x, JACOBI_KERNEL)
-        x2 = (s + h2 * rhs) / numpy.maximum(1e-3, n)
-        x = numpy.where(checkerboard == c, x2, x)
+        s = convolve_jacobi(b * x)
+        x2 = (s + h2 * rhs) / np.maximum(1e-3, n)
+        x = np.where(checkerboard == c, x2, x)
     return x
 
 
 def mg_opt(
-    x: numpy.ndarray,
-    rhs: numpy.ndarray,
-    b: numpy.ndarray,
+    x: np.ndarray,
+    rhs: np.ndarray,
+    b: np.ndarray,
     mode: MultigridMode = MultigridMode.VCycle,
-    n: typing.Optional[numpy.ndarray] = None,
+    n: np.ndarray | None = None,
     h: float = 1.0,
-) -> numpy.ndarray:
+    options: MultigridOptions | None = None,
+) -> np.ndarray:
+
+    options = options or MultigridOptions()
 
     if any(d < 8 for d in x.shape[:2]):
         return x
-    
+
     if n is None:
-        n = convolve(b, JACOBI_KERNEL)
+        n = convolve_jacobi(b)
+
+    smooth = smooth_gsrb if options.gsrb else smooth_jacobi
 
     # pre-smooth
-    for _ in range(2):
-        x = smooth_gsrb(x, rhs, b, n, h*h)
+    for _ in range(options.presmooth):
+        x = smooth(x, rhs, b, n, h*h)
 
     for sub_mode in SUB_CYCLES[mode]:
         r = residual(x, rhs, b, n, h)
-        e = _coarse_grid_correction(r, b, sub_mode, h)
+        e = _coarse_grid_correction(r, b, sub_mode, h, options)
         x += e
 
-        for _ in range(2):
-            x = smooth_gsrb(x, rhs, b, n, h*h)
+        for _ in range(options.postsmooth):
+            x = smooth(x, rhs, b, n, h*h)
 
     return x
 
 def residual(
-    x: numpy.ndarray,
-    rhs: numpy.ndarray,
-    b: numpy.ndarray,
-    n: typing.Optional[numpy.ndarray] = None,
+    x: np.ndarray,
+    rhs: np.ndarray,
+    b: np.ndarray,
+    n: typing.Optional[np.ndarray] = None,
     h: float = 1.0,
-) -> numpy.ndarray:
-
+) -> np.ndarray:
 
     if n is None:
-        n = convolve(b, JACOBI_KERNEL)
+        n = convolve_jacobi(b)
 
-    s = convolve(b * x, JACOBI_KERNEL)
+    s = convolve_jacobi(b * x)
     L = n * x - s
     r = rhs - L / (h * h)
     return r
 
 def _coarse_grid_correction(
-    r: numpy.ndarray,
-    b: numpy.ndarray,
+    r: np.ndarray,
+    b: np.ndarray,
     mode: MultigridMode,
     h: float,
-) -> numpy.ndarray:
+    options: MultigridOptions,
+) -> np.ndarray:
 
-    ### RESTRICTION
+    # RESTRICT
+    block_sizes = (options.reduction, options.reduction)
 
-    # bilinear
-    # rc = restrict(r, b)
-    block_sizes = (2, 2) if len(r.shape) == 2 else (2, 2, 1)
-    rc = skimage.measure.block_reduce(r * b, block_sizes, numpy.mean)
-    bc = skimage.measure.block_reduce(b, block_sizes, numpy.mean)
-
-    # rc = skimage.transform.downscale_local_mean(b * r, (2, 2))
-    # bc = skimage.transform.downscale_local_mean(b, (2, 2))
-
-    # sc = (np.asarray(r.shape) + 1) // 2
-    # rc = resize(b * r, sc)
-    # bc = resize(b, sc)
-
-    nc = convolve(bc, JACOBI_KERNEL)
+    if options.mean_reduction:
+        rc = block_reduce(r * b, block_sizes, np.mean)
+        bc = block_reduce(b, block_sizes, np.mean)
+    else:
+        rc = (r * b)[::2, ::2]
+        bc = b[::2, ::2]
+    nc = convolve_jacobi(bc)
 
     # RECURSE
-    ec = mg_opt(numpy.zeros_like(rc), rc, bc, mode, nc, 2 * h)
+    ec = mg_opt(np.zeros_like(rc), rc, bc, mode, nc, options.reduction * h, options)
 
-    ### PROLONGATE
-
-    # naive
+    # PROLONGATE
     e = prolongate(ec, block_sizes)
-    # e = skimage.transform.resize_local_mean(ec, np.multiply(2, r.shape))
-    # e = skimage.transform.rescale(ec, 2)
-    # e = resize(ec, numpy.multiply(r.shape, 2))
-
     e = e[:r.shape[0], :r.shape[1]]
-
-
-    # optimized
-    # e = cy_prolongate(ec, r.shape)
-
-    # correct
     return e
 
 
 def divergence(
-    x: numpy.ndarray,
-    b: numpy.ndarray,
-) -> numpy.ndarray:
+    x: np.ndarray,
+    b: np.ndarray,
+) -> np.ndarray:
 
-    return convolve(b * x, JACOBI_KERNEL) - convolve(b, JACOBI_KERNEL) * x
+    return convolve_jacobi(b * x) - convolve_jacobi(b) * x
